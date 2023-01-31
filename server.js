f377ded69e41216b3fee6dc5948fbb4e81bcc0fa
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-31 11:18:36 +0545

    changes

--- server.js
+++ server.js
@@ -31,9 +31,11 @@ app.get('*', (req, res) => {
 })
 
 // listen
-app.listen(5000, () => {
-  console.log(`Server is running on port 5000`.yellow.bold);
-});
+connectDb().then(() => {
+  app.listen(5000, () => {
+    console.log(`Server is running on port 5000`.yellow.bold);
+  });
+})
 
 
 //
