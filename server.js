6a2cfd819d734beea2485ff9de61e86e4844886b
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-31 11:30:58 +0545

    changes

--- server.js
+++ server.js
@@ -30,9 +30,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, "./client/dist/index.html"))
 })
 
+const PORT = process.env.PORT || 5000;
+
 // listen
 connectDb().then(() => {
-  app.listen(7000, () => {
+  app.listen(PORT, () => {
     console.log(`Server is running on port 5000`.yellow.bold);
   });
 })
